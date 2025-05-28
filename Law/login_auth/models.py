363005ce4c442310cf7ed2d23372f794dfbcadac
ASUR12ZURVAN <143814906+ASUR12ZURVAN@users.noreply.github.com>
2025-05-28 19:45:27 +0530

    Better UI implementations

--- Law/login_auth/models.py
+++ Law/login_auth/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    email_id = models.EmailField(unique=True)
+    password = models.CharField(max_length=200)
+
+    def __str__(self):
+        return User.name
+
